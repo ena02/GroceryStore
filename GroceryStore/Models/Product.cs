1f3564ce30ccba0bd542cb2c05dc172045bcebce
ena <erkembekov.nurzhan@mail.ru>
2024-10-05 21:38:40 +0500

    add Product model

---
+++ GroceryStore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryStore.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
