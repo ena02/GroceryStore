9d5cde9e58ea1ba121bbcfbd75f1a627ec383d7a
ena <erkembekov.nurzhan@mail.ru>
2024-10-06 18:51:02 +0500

    add repo

---
+++ GroceryStore/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GroceryStore.Models;
+
+namespace GroceryStore.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> CreateAsync(Product product);
+        Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize);
+        Task<Product> GetByIdAsync(int id);
+        Task<bool> UpdateAsync(Product product);
+        Task<bool> DeleteAsync(int id);
+        Task<int> GetTotalCountAsync();
+    }
+}
---
+++ GroceryStore/Repositories/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using GroceryStore.Data;
+using GroceryStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryStore.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return false;
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize)
+        {
+            return await _context.Products
+                .OrderBy(p => p.ID)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<int> GetTotalCountAsync()
+        {
+            return await _context.Products.CountAsync();
+        }
+
+        public async Task<bool> UpdateAsync(Product product)
+        {
+            var existingProduct = await _context.Products.FindAsync(product.ID);
+            if (existingProduct == null)
+                return false;
+
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            // Обновление даты добавления, если необходимо
+            // existingProduct.CreatedAt = product.CreatedAt;
+
+            _context.Products.Update(existingProduct);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
