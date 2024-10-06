0de041c6d4b4a9be485c4ebbab7b96fa21e474d2
ena <erkembekov.nurzhan@mail.ru>
2024-10-06 19:12:34 +0500

    add logging(middleware)

---
+++ GroceryStore/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GroceryStore.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Произошла непредвиденная ошибка.");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var response = new { Message = "Внутренняя ошибка сервера." };
+                var json = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
